66af0063adb1c89681b79015aa35bb959748b0ec
Matiss Janis Aboltins <amatiss@amazon.com>
2020-02-08 16:33:40 +0000

    Updates

---
+++ precache-manifest.4877b36f4b6362b8e11faa87df579953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a5d80d7f708ce790a5eaa2ac2cf80c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b1d640f61b193d14ce8",
+    "url": "/static/css/main.3e52f998.chunk.css"
+  },
+  {
+    "revision": "efd6f2937e829c135241",
+    "url": "/static/js/2.fb2a8b97.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fb2a8b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1d640f61b193d14ce8",
+    "url": "/static/js/main.54d66b3b.chunk.js"
+  },
+  {
+    "revision": "0f48c23d227873aacf2a",
+    "url": "/static/js/runtime-main.f282939c.js"
+  }
+]);
